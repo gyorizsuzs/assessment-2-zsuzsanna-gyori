7fd11458421d9a632b1b6009dd8631ad6a544b2c
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-11-29 13:34:29 +0100

    adding pagination, 10 user per page

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,6 +1,5 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import ReactPaginate from 'react-paginate';
 import { Button } from '@mui/material';
 
 import LoadingMask from '../../components/loading/loading.component';
@@ -11,7 +10,8 @@ import './home.styles.css';
 function Home() {
   const [users, setUsers] = useState();
   const [loading, setLoading] = useState(true);
-  /* const [page, setPage] = useState(); */
+  const [firstUser, setFirstUser] = useState(0);
+  const [lastUser, setLastUser] = useState(10);
 
   useEffect(() => {
     fetch(`https://assessment-users-backend.herokuapp.com/users.json`, {
@@ -23,95 +23,30 @@ function Home() {
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        /* console.log(data); */
         setUsers(data);
         setLoading(false);
       });
-  }, []);
-
-  /* function Users({ users }) {
-    return (
-      <Fragment>
-        {users && users.map((user, index) => <User key={index} user={user} />)}
-      </Fragment>
-    );
-  }
-
-  function PaginatedUsers({ usersPerPage }) {
-    const [userOffset, setUserOffset] = useState(0);
-    const endOffset = userOffset + usersPerPage;
-    const currentUsers = users.slice(userOffset, endOffset);
-    const pageCount = Math.ceil(users.length / usersPerPage);
-
-    const handlePageChange = (event) => {
-      const newOffset = (event.selected * usersPerPage) % users.length;
-      setUserOffset(newOffset);
-    };
-
-    return (
-      <Fragment>
-        <Users currentUsers={currentUsers} />
-        <ReactPaginate
-          breakLabel='...'
-          nextLabel='next >'
-          onPageChange={handlePageChange}
-          pageRangeDisplayed={5}
-          pageCount={pageCount}
-          previousLabel='< previous'
-          renderOnZeroPageCount={null}
-        />
-      </Fragment>
-    );
-  } */
-
-  /*   const pages = [
-    users
-      .slice([0], [10])
-      .map((user, index) => <User key={index} user={user} />),
-    users
-      .slice([11], [20])
-      .map((user, index) => <User key={index} user={user} />),
-    users
-      .slice([21], [30])
-      .map((user, index) => <User key={index} user={user} />),
-    users
-      .slice([31], [40])
-      .map((user, index) => <User key={index} user={user} />),
-    users
-      .slice([41], [50])
-      .map((user, index) => <User key={index} user={user} />),
-    users
-      .slice([51], [60])
-      .map((user, index) => <User key={index} user={user} />),
-    users
-      .slice([61], [70])
-      .map((user, index) => <User key={index} user={user} />),
-    users
-      .slice([71], [80])
-      .map((user, index) => <User key={index} user={user} />),
-    users
-      .slice([81], [90])
-      .map((user, index) => <User key={index} user={user} />),
-  ]; */
-
-  /*   function handlePrev() {
-    setPage(0);
-  }
-  function handleNext() {
-    setPage(1);
-  } */
-
-  let first = 0;
-  let last = 10;
+  }, [firstUser, lastUser]);
 
   function handlePrev() {
-    if (first > 0) {
-      return (first - 10) & (last - 10);
+    if (firstUser > 1) {
+      setFirstUser(firstUser - 10);
+      setLastUser(lastUser - 10);
+    } else {
+      setFirstUser(0);
+      setLastUser(10);
     }
   }
 
   function handleNext() {
-    return (first + 10) & (last + 10);
+    if (lastUser < 90) {
+      setFirstUser(firstUser + 10);
+      setLastUser(lastUser + 10);
+    } else {
+      setFirstUser(80);
+      setLastUser(90);
+    }
   }
 
   return (
@@ -119,17 +54,15 @@ function Home() {
       <h1 className='title'>Users</h1>
       <div className='main-container'>
         <div className='inner-container'>
-          {!users ? (
+          {loading ? (
             <LoadingMask />
           ) : (
             users
-              .slice(first, last)
+              .slice(firstUser, lastUser)
               .map((user, index) => <User key={index} user={user} />)
-            /* pages[0] */
           )}
         </div>
         <div className='button-container'>
-          {/* <Pagination count={9} /> */}
           <Button onClick={handlePrev}>prev</Button>
           <Button onClick={handleNext}>next</Button>
         </div>
